f0c0961bb7c0ec54d7025083aab64e8075b07323
laverde 2723 <juanklaverde03@gmail.com>
2023-03-02 22:51:30 -0500

    :D3

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ const check = 'fa-check-circle'
 const uncheck = 'fa-circle'
 const lineThrough = 'line-through'
 let id = 0
+const LIST = []
+
+
+
+
+//creacion de fecha
+
+const FECHA = new Date()
+fecha.innerHTML = FECHA.toLocaleDateString('es-MX', {weekday: 'long', month:'long', day:'numeric'})
+
+
+
+
+
 
 
 //funcion agregar tarea
@@ -30,10 +44,39 @@ function agergarTarea(tarea, id, realizado, eliminado){
 }
 
 
+//funcion de tarea relizada
+
+//parentNode = identifica los elementos hijos, "Li" es el papa de "p", donde se modifica el texto, poniendo la linea
+
+function tareaRealizada(element){
+    element.classList.toggle(check)
+    element.classList.toggle(uncheck)
+    element.parentNode.querySelector('.text').classList.toggle(lineThrough)
+    LIST[element.id].realizado = LIST[element.id].realizado ?false :true
+}
+
+//funcion de tarea eliminada
+
+function tareaEliliminada(element){
+    element.parentNode.parentNode.removeChild(element.parentNode)
+    LIST[element.id].eliminado = true
+}
+
+
+
+
+
+
 botonEnter.addEventListener('click', ()=> {
     const tarea = input.value
     if(tarea){
         agergarTarea(tarea, id, false, false)
+        LIST.push({
+            nombre: tarea,
+            id: id,
+            realizado: false,
+            eliminado: false
+        })
     }
     input.value = ''
     id++
@@ -44,6 +87,12 @@ document.addEventListener('keyup', function(event){
         const tarea = input.value
         if(tarea){
             agergarTarea(tarea, id, false, false)
+            LIST.push({
+                nombre: tarea,
+                id: id,
+                realizado: false,
+                eliminado: false
+            })
         }
         input.value = ''
         id++
@@ -53,4 +102,10 @@ document.addEventListener('keyup', function(event){
 lista.addEventListener('click', function(event){
     const element = event.target
     const elementData = element.attributes.data.value
+    if(elementData==='realizado'){
+        tareaRealizada(element)
+    }
+    else if (elementData==='eliminado'){
+        tareaEliliminada(element)
+    }
 })
